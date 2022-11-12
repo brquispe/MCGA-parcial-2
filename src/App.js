949923f7295f9ae631c46ca6b76a9d60d93ebbfb
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-12 05:06:04 -0300

    ✨ Use layout in all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./shared/components/Layout";
 
 const App = () => {
   return (
+    <Layout>
       <Routes>
         <Route path="/" element={<div>Hello, world!</div>} />
       </Routes>
+    </Layout>
   );
 }
 
