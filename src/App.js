1844accbc721d5fe6d969ecb08292f55eba2303e
Horacio Lerda <horacio.lerda@gmail.com>
2022-11-12 16:26:42 -0300

    horacio-06: Provider and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./shared/components/Layout";
 import Home from "./shared/pages/Home";
+import Products from './shared/pages/Products';
 
 const App = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products/>} />
       </Routes>
     </Layout>
   );
