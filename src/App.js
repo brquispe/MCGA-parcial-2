7e769131fa888fb7ed37bea4e7f732edd932553a
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-12 09:21:15 -0300

    🍱 Connect Home page to route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./shared/components/Layout";
+import Home from "./shared/pages/Home";
 
 const App = () => {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<div>Hello, world!</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Layout>
   );
