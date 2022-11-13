f85e8276cc2d98d2762ce065f2d19eaf788b7b00
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 08:20:05 -0300

    🔨 Improve error handling

--- src/Products/store/thunks.js
+++ src/Products/store/thunks.js
@@ -7,8 +7,8 @@ export const getProducts = () => async (dispatch) => {
   let response = await fetch(`${pURL}/products`);
   const prod = await response?.json();
   if (response.status === 200) {
-    dispatch(setProducts(prod.data));
     dispatch(dismissLoading());
+    dispatch(setProducts(prod.data));
     return;
   } else {
     dispatch(dismissLoading());
@@ -20,21 +20,26 @@ export const getProducts = () => async (dispatch) => {
  * @param {{ name: string; price: number; providerId?: string }} data
  */
 export const addProduct = (data) => async (dispatch) => {
-  dispatch(setLoading());
-  let response = await fetch(`${pURL}/products`, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (!response.ok) {
+  try {
+    dispatch(setLoading());
+    let response = await fetch(`${pURL}/products`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      dispatch(dismissLoading());
+      return;
+    }
+    const product = await response?.json();
     dispatch(dismissLoading());
-    return;
+    dispatch(getProducts());
+    return product;
+  } catch (error) {
+    dispatch(dismissLoading);
   }
-  const product = await response?.json();
-  dispatch(getProducts());
-  return product;
 };
 
 /**
@@ -42,29 +47,35 @@ export const addProduct = (data) => async (dispatch) => {
  * @param {{ name?: string; price?: number; providerId?: string }} data
  */
 export const updateProduct = (productId, data) => async (dispatch) => {
-  dispatch(setLoading());
-  let response = await fetch(`${pURL}/products/${productId}`, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-  });
-  if (!response.ok) {
+  try {
+    dispatch(setLoading());
+    let response = await fetch(`${pURL}/products/${productId}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      dispatch(dismissLoading());
+      return;
+    }
+    const product = await response?.json();
     dispatch(dismissLoading());
-    return;
+    dispatch(getProducts());
+    return product;
+  } catch (error) {
+    dispatch(dismissLoading());
   }
-
-  const product = await response?.json();
-  dispatch(getProducts());
-  return product;
 };
 
 export const removeProduct = (productId) => async (dispatch) => {
   try {
     dispatch(setLoading());
-    const response = await fetch(`${pURL}/products/${productId}`, { method: 'DELETE' });
+    const response = await fetch(`${pURL}/products/${productId}`, {
+      method: 'DELETE',
+    });
     dispatch(dismissLoading());
     dispatch(getProducts());
-    return response.status === 204
+    return response.status === 204;
   } catch (error) {
     dispatch(dismissLoading());
   }
-}
+};
