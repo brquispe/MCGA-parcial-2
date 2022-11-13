b547a5696798c7100192e4fcfdbfa3089fa18eea
Horacio Lerda <horacio.lerda@gmail.com>
2022-11-13 12:39:47 -0300

    horacio-10: preliminary implementation

--- src/shared/pages/Products/index.js
+++ src/shared/pages/Products/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProducts } from "../../redux/Products/thunks";
+import ProdForm from "../../../Products/components/prodForm";
+import styles from './prod.module.css';
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -21,6 +23,20 @@ if (isLoadingProducts) return <div>cargando</div>
    return (
     <div>
         <h1>Productos</h1>
+        <div className={styles.container}>
+          <ProdForm/>
+        </div>
+        <table>    
+        <tr><th>Id Code</th><th>Name</th><th>Price</th><th>Provider</th></tr>
+        {products.map((product) =>         
+          <tr>
+            <td>{product._id}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.provider.name}</td>
+          </tr>
+        )}      
+      </table>
     </div>
    )
    }
