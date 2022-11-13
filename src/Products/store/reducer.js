8931b28e7e78d5100417e1b1f991f35da5aafdc7
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 17:46:45 -0300

    💡 Add JSDoc comments to products reducer

--- src/Products/store/reducer.js
+++ src/Products/store/reducer.js
@@ -1,10 +1,21 @@
+import { useSelector } from 'react-redux';
 import { DISMISS_LOADING, SET_LOADING, SET_PRODUCTS } from './types';
 
+/**
+ * @typedef {Readonly<{products: { _id: string; name: string; price: string; provider: {_id: string} }[]; isLoadingProducts: boolean}>} ProductsState
+ */
+
+/**
+ * @type ProductsState
+ */
 const INITIAL_STATE = {
   products: [],
   isLoadingProducts: false,
 };
 
+/**
+ * @param {{ type: string; payload: unknown }} action
+ */
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_PRODUCTS:
