ce668918e832ddb0df2f89f4825466a23062a601
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-14 19:57:17 -0300

    ✏️ Fix home page typo

--- src/shared/pages/Home.js
+++ src/shared/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return <div className={styles.home}>
     <h1>Bienvenido</h1>
     <h2>Universidad Abierta Interamericana</h2>
-    <h3>MCGA 2 - Lerda y Quispe</h3>
+    <h3>MCGA Parcial 2 - Lerda y Quispe</h3>
   </div>
 };
 
