53bb1ae84c13786247f2cacc8eb1c149952d9353
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 22:50:58 -0300

    🍱 Add small size for loading spinner

--- src/shared/components/LoadingSpinner/index.js
+++ src/shared/components/LoadingSpinner/index.js
@@ -1,8 +1,8 @@
 import styles from './LoadingSpinner.module.css';
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ size = 'normal' }) => {
   return (
-    <div className={styles['lds-ring']}>
+    <div className={`${styles['lds-ring']} ${size === 'small' ? styles['ls-small'] : ''}`}>
       <div></div>
       <div></div>
       <div></div>
