0b57291e68c76902f2915a5df05c442d481173d6
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 22:58:30 -0300

    🔨 Add title to create product page

--- src/shared/pages/Product/index.js
+++ src/shared/pages/Product/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ProdForm from "../../../Products/components/prodForm";
+import ProdForm from '../../../Products/components/prodForm';
 import styles from './product.module.css';
 
 const ProdInputForm = () => {
-    return (
-      <div className={styles.form}>
-        <div className={styles.divForm}>FORMULARIO</div>
-        <ProdForm/>
-      </div>
-    );
-  };
-  
-  export default ProdInputForm;
+  document.title = 'Agregar producto';
+
+  return (
+    <div className={styles.form}>
+      <h1 className={styles.divForm}>Agregar producto</h1>
+      <ProdForm />
+    </div>
+  );
+};
+
+export default ProdInputForm;
