57ed280f4e35663a2924436e2db4d0ffc70e9e38
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 21:22:34 -0300

    💄 Add class to layout component

--- src/shared/components/Layout/index.js
+++ src/shared/components/Layout/index.js
@@ -5,7 +5,7 @@ import Header from "./Header/Header";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header/>
       <Body>
         {children}
