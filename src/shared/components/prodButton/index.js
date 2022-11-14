74b88bf90e4597d30b99756c66ee4f42881769eb
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 22:52:12 -0300

    🍱 Add button style and loading state

--- src/shared/components/prodButton/index.js
+++ src/shared/components/prodButton/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import LoadingSpinner from '../LoadingSpinner';
+import styles from './Button.module.css';
 
-const sharedButton = ({text, onClick, type}) => {
+const sharedButton = ({text, onClick, type, loading, className}) => {
     return (
-      <button type={type} onClick={onClick}>
-        {text}
+      <button type={type} onClick={onClick} disabled={loading} className={`${styles.btn} ${className}`}>
+        <LoadingSpinner size="small" />
+        <span>{text}</span>
       </button>
     );
   };
