a1b9ebebd5afe269bf42d4eb8e5ab3e64de2abf5
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 23:07:04 -0300

    🔨 Make add product form work

--- src/Products/components/prodForm/index.js
+++ src/Products/components/prodForm/index.js
@@ -1,55 +1,87 @@
-import React from 'react';
+import { useEffect } from 'react';
 import styles from './prodform.module.css';
 import ProdInput from '../../../shared/components/prodInput';
 import ProdButton from '../../../shared/components/prodButton';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useProducts } from 'Products/store/reducer';
+import { addProduct, getProviders } from 'Products/store/thunks';
 
 const ProdForm = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  
-    // const userWatch = watch('userName')
-    // const passWatch = watch('Password')
-    
-    //change logic
-    const navigate = useNavigate()
-    const submission = (data) => {
-      console.log(data);
-      navigate("/products")
-    }
-    //change logic
-    
-    // console.log(userWatch, passWatch)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    return (
-      <form className={styles.form} onSubmit={handleSubmit(submission)}>
-        <ProdInput register={register} type='text' placeholder='DESCRIPCION' name='descProd'
-          rules={
-            {
-              required: 'Se requiere este valor',
-              pattern:
-                { minlength: 5 , message: "No cumple con la el patron" }
-             }} />
-        {errors.userName?.message}
+  const dispatch = useDispatch();
+  const { providers, isLoadingProducts } = useProducts();
 
-        <ProdInput register={register} type='number' placeholder='PRECIO' name='precioProd'
-          rules={
-            {
-              required: 'Se requiere este valor',
-              pattern:
-                { value: /^[a-zA-Z0-9\.]*$/, message: "Debe ser un numero con decimal" }
-             }} />
-        {errors.userName?.message}
+  useEffect(() => {
+    dispatch(getProviders());
+  }, [dispatch]);
 
-        <ProdInput register={register} type='text' placeholder='PROVEEDOR' name='provider'
-          rules={
-            { 
-              required: 'contraseña obligatoria' 
-              }} />
-        {errors.Password?.message}
-        <ProdButton text='Submit' onClick={() => null} type='submit' />
-      </form>
-    );
+  const navigate = useNavigate();
+
+  const submission = (product) => {
+    dispatch(addProduct(product));
+    navigate('/products');
   };
 
-  export default ProdForm;
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(submission)}>
+      <ProdInput
+        register={register}
+        type="text"
+        placeholder="Nombre"
+        name="name"
+        label="Nombre"
+        rules={{
+          required: 'Se requiere este valor',
+          pattern: { minlength: 5, message: 'No cumple con la el patron' },
+        }}
+      />
+      {errors.name?.message}
+
+      <ProdInput
+        register={register}
+        type="number"
+        placeholder="Precio"
+        name="price"
+        label="Precio"
+        rules={{
+          required: 'Se requiere este valor',
+          pattern: {
+            value: /^[a-zA-Z0-9\.]*$/,
+            message: 'Debe ser un numero con decimal',
+          },
+        }}
+      />
+      {errors.price?.message}
+
+      <select
+        name="providerId"
+        id="provider"
+        {...register('providerId', { required: 'El proveedor es requerido' })}
+      >
+        <option></option>
+        {providers?.map((provider) => (
+          <option id={provider._id} key={provider._id} value={provider._id}>
+            {provider.name}
+          </option>
+        ))}
+      </select>
+      {errors.providerId?.message}
+
+      <ProdButton
+        text="Guardar"
+        onClick={() => null}
+        type="submit"
+        loading={isLoadingProducts}
+      />
+    </form>
+  );
+};
+
+export default ProdForm;
