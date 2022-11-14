a9fa8811af077c3b6db5bc70bcaae5375014a5c7
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-14 19:21:38 -0300

    💄 Improve shared input style

--- src/shared/components/prodInput/index.js
+++ src/shared/components/prodInput/index.js
@@ -1,15 +1,18 @@
-const ProdInput = ({ register, type, placeholder, name, rules, label }) => {
+import styles from './Input.module.css';
+
+const ProdInput = ({ register, type, placeholder, name, rules, label, className, ...props }) => {
   return (
-    <>
+    <div className={`${styles.formControl} ${className || ''}`}>
       <label htmlFor={name}>{label || placeholder}</label>
       <input
+        {...props}
         {...register(`${name}`, rules)}
         type={type}
         placeholder={placeholder}
         name={name}
         id={name}
       />
-    </>
+    </div>
   );
 };
 
