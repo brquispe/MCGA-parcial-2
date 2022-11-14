2c4a5f5088bd53c2446ae368b9c6e6a35b323675
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-13 22:56:52 -0300

    🍱 Update loading spinner state in button

--- src/shared/components/prodButton/index.js
+++ src/shared/components/prodButton/index.js
@@ -4,8 +4,8 @@ import styles from './Button.module.css';
 const sharedButton = ({text, onClick, type, loading, className}) => {
     return (
       <button type={type} onClick={onClick} disabled={loading} className={`${styles.btn} ${className}`}>
-        <LoadingSpinner size="small" />
-        <span>{text}</span>
+        {loading && <LoadingSpinner size="small" />}
+        <span className={styles.btn__label}>{text}</span>
       </button>
     );
   };
